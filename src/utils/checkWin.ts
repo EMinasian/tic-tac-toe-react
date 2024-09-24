b29fa3e70aa80af8f79d863ca1868a34d9a7815d
erik.minasian <eminasian@webmd.net>
2024-09-24 11:19:24 +0400

    feat: symbol type for check winner

--- src/utils/checkWin.ts
+++ src/utils/checkWin.ts
@@ -1,7 +1,7 @@
 import { VICTORY_PATTERNS } from "./constants";
-import { CellsType } from "./types/types";
+import { CellsType, PlayerSymbolType } from "./types/types";
 
-export default function checkWin(potentialWinner: string, cells: CellsType): string | null {
+export default function checkWin(potentialWinner: PlayerSymbolType, cells: CellsType): PlayerSymbolType | null {
   for (const pattern of VICTORY_PATTERNS) {
     let hasWinner = true;
     for (const point of pattern) {
